4ff9578ff1a803a1567a092342e7304b64609295
Andrey Kudryavtsev <tmac1attack@gmail.com>
2018-09-25 14:37:17 +0300

    Unit tests added

---
+++ StopwatchTest/LapsTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyStopwatch;
+
+namespace StopwatchTest
+{
+    [TestClass]
+    public class LapsTest
+    {
+        [TestMethod]
+        public void InitializationTest()
+        {
+            // Arrange & Act
+            ViewModel vm = new ViewModel();
+            // Assert
+            Assert.AreEqual(0, vm.Laps.Count);
+        }
+
+        [TestMethod]
+        public void LapAdditionTest()
+        {
+            // Arrange
+            ViewModel vm = new ViewModel();
+            // Act
+            vm.AddLap.Execute("00:00:01.000");
+            // Assert
+            Assert.AreEqual("1. 00:00:01.000", vm.Laps[0].LapEntry);
+        }
+
+        [TestMethod]
+        public void AdditionAvailabilityTest()
+        {
+            // Arrange
+            ViewModel vm = new ViewModel();
+            // Act
+            vm.AddLap.Execute("00:00:01.000");
+            // Assert
+            Assert.IsFalse(vm.AddLap.CanExecute("00:00:01.000"));
+        }
+
+        [TestMethod]
+        public void AdditionAvailabilityTest2()
+        {
+            // Arrange
+            ViewModel vm = new ViewModel();
+            // Act
+
+            // Assert
+            Assert.IsTrue(vm.AddLap.CanExecute("00:00:01.000"));
+        }
+
+        [TestMethod]
+        public void StopWatchStartTest()
+        {
+            // Arrange
+            ViewModel vm = new ViewModel();
+            // Act
+            vm.StartCommand.Execute(true);
+            // Assert
+            Assert.IsTrue(vm.StartPauseButtonText == "Pause");
+        }
+
+        [TestMethod]
+        public void StopWatchPauseTest()
+        {
+            // Arrange
+            ViewModel vm = new ViewModel();
+            // Act
+            vm.StartCommand.Execute(true);
+            vm.StartCommand.Execute(true);
+            // Assert
+            Assert.IsTrue(vm.StartPauseButtonText == "Resume");
+        }
+
+        [TestMethod]
+        public void ResetTest()
+        {
+            // Arrange
+            ViewModel vm = new ViewModel();
+            // Act
+            vm.AddLap.Execute("00:00:01.000");
+            vm.AddLap.Execute("00:00:02.000");
+            vm.ResetCommand.Execute(true);
+            // Assert
+            Assert.IsTrue(vm.Laps.Count == 0);
+        }
+    }
+}
---
+++ StopwatchTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StopwatchTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StopwatchTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7a5ddbb8-a046-4d1a-a8d1-ef16463afd3d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
